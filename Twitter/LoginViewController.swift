19655b8019f76ca49a3b244efd40fbc845e3f6c6
ezana tesfaye <ezana.tesfaye@sjsu.edu>
2019-02-28 23:47:36 -0800

    tweet, retweet and favorite now working

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Ezana Tesfaye on 2/21/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Couldnt login")
+        })
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Ezana Tesfaye on 2/28/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    @IBAction func cancel(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet")
+                self.dismiss(animated: true, completion: nil)
+            })
+            
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
